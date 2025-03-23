c20b1e8a64dc062047d51b8666527ed96dd74fc5
HirunaVishwamith <hvishwamith@gmail.com>
2025-03-23 20:36:56 +0530

    fix the ammoadd instuction

--- common/util.h
+++ common/util.h
@@ -34,66 +34,66 @@ static int verify(int n, const volatile int *test, const int *verify)
   return 0;
 }
 
-// // Atomic fetch-and-add using RISC-V atomic instruction
-// static inline int atomic_fetch_add(volatile int *ptr, int val)
-// {
-//   int old;
-//   asm volatile(
-//       "amoadd.w.aqrl %0, %2, (%1)" // Atomic add with acquire-release semantics
-//       : "=r"(old)
-//       : "r"(ptr), "r"(val)
-//       : "memory");
-//   return old;
-// }
-
-// static void __attribute__((noinline)) barrier(int ncores)
-// {
-//   static volatile int sense;
-//   static volatile int count;
-//   static __thread int threadsense; // non shared variable in TLS
-
-//   __sync_synchronize(); // fence
-
-//   threadsense = !threadsense;
-
-//   int old_count = atomic_fetch_add(&count, 1); // Atomically increment count
-//   if (old_count == ncores - 1)
-//   {
-//     // Last thread to arrive
-//     count = 0;           // Reset counter for next barrier
-//     sense = threadsense; // Update global sense to this thread's sense
-//   }
-//   else
-//   {
-//     // Other threads wait for global sense to match their local sense
-//     while (sense != threadsense)
-//     {
-//       // Busy wait (could optimize with a yield if available)
-//     }
-//   }
-
-//   __sync_synchronize();
-// }
+// Atomic fetch-and-add using RISC-V atomic instruction
+static inline int atomic_fetch_add(volatile int *ptr, int val)
+{
+  int old;
+  asm volatile(
+      "amoadd.w.aqrl %0, %2, (%1)" // Atomic add with acquire-release semantics
+      : "=r"(old)
+      : "r"(ptr), "r"(val)
+      : "memory");
+  return old;
+}
 
 static void __attribute__((noinline)) barrier(int ncores)
 {
   static volatile int sense;
   static volatile int count;
-  static __thread int threadsense;  // non shared variable in TLS
+  static __thread int threadsense; // non shared variable in TLS
 
-  __sync_synchronize();   // fence
+  __sync_synchronize(); // fence
 
   threadsense = !threadsense;
-  if (__sync_fetch_and_add(&count, 1) == ncores-1)
+
+  int old_count = atomic_fetch_add(&count, 1); // Atomically increment count
+  if (old_count == ncores - 1)
   {
-    count = 0;
-    sense = threadsense;
+    // Last thread to arrive
+    count = 0;           // Reset counter for next barrier
+    sense = threadsense; // Update global sense to this thread's sense
   }
-  else while(sense != threadsense)
-    ;
+  else
+  {
+    // Other threads wait for global sense to match their local sense
+    while (sense != threadsense)
+    {
+      // Busy wait (could optimize with a yield if available)
+    }
+  }
 
   __sync_synchronize();
 }
+
+// static void __attribute__((noinline)) barrier(int ncores)
+// {
+//   static volatile int sense;
+//   static volatile int count;
+//   static __thread int threadsense;  // non shared variable in TLS
+
+//   __sync_synchronize();   // fence
+
+//   threadsense = !threadsense;
+//   if (__sync_fetch_and_add(&count, 1) == ncores-1)
+//   {
+//     count = 0;
+//     sense = threadsense;
+//   }
+//   else while(sense != threadsense)
+//     ;
+
+//   __sync_synchronize();
+// }
 
 // this needs to be enabled to calculate IPC and CPI
 // #ifdef __riscv
